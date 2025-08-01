4f6cab2b41f88e246f658d974bcf39f691124116
George <g.murashov@inbox.ru>
2025-08-01 19:43:15 +0300

    delete type in training

--- test_script/test.go
+++ test_script/test.go
@@ -1,11 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"runtime"
+	"sync"
+	"time"
+)
 
-func main() {
-	ch := make(chan int)
-
-	fmt.Println(ch)
+func worker(ctx context.Context, task <-chan int, resultCh chan<- int, id int) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case v, ok := <-task:
+			if !ok {
+				return
+			}
+			resultCh <- v * v
+		}
+	}
 }
 
-// посмотреть и прорешать вместе с автором про каналы(buff unbuff)
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*20)
+	defer cancel()
+
+	wg := &sync.WaitGroup{}
+
+	task, resultCh := make(chan int, 5), make(chan int, 5)
+
+	for i := 0; i <= runtime.NumCPU(); i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			worker(ctx, task, resultCh, i)
+		}()
+	}
+
+	go func() {
+		for i := 0; i < 1000; i++ {
+			task <- i
+		}
+		close(task)
+	}()
+
+	go func() {
+		wg.Wait() // --- блокируется горутина ---
+		close(resultCh)
+	}()
+
+	for res := range resultCh {
+		fmt.Println(res)
+	}
+}
+
+// 22 урок
