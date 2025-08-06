4bd86e42058020fc0f914dcebfecb889ab591f1c
George <92686556+GrudTrigger@users.noreply.github.com>
2025-08-06 14:34:46 +0300

    pull main

--- test_script/test.go
+++ test_script/test.go
@@ -1,11 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
 
 func main() {
-	ch := make(chan int)
+	ch := make(chan string)
 
-	fmt.Println(ch)
+	go func(){
+		for i := range 10000 {
+			time.Sleep(300 * time.Millisecond)
+			ch <- fmt.Sprintf("value = %d", i)
+		}
+		close(ch)
+	}()
+
+	go func (){
+		for v:= range ch {
+			fmt.Println(v)
+		}
+	}()
+ // |(main)   |(g1)   |(g2)
+	for range 100 {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println("Выполняется какой-то следующий код, пока в фоне работает асинхронность")
+	}
 }
 
 // посмотреть и прорешать вместе с автором про каналы(buff unbuff)
