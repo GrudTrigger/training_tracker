82a6b9a682a1a33a37d2971c07cf3936fac42336
George <92686556+GrudTrigger@users.noreply.github.com>
2025-12-11 12:04:11 +0300

    actions:test linj

--- backend/internal/service/exercise/update.go
+++ backend/internal/service/exercise/update.go
@@ -2,10 +2,12 @@ package exercise
 
 import (
 	"context"
+	"fmt"
 
 	model "github.com/GrudTrigger/training_tracker/backend/gen/exercise"
 )
 
 func (s *ExerciseService) Update(ctx context.Context, data *model.UpdatePayload) (*model.ExerciseList, error) {
+	fmt.Println("123")
 	return &model.ExerciseList{}, nil
 }
