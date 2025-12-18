b87f6f7be82e09bc4deb466cab3ccbdadfa56c51
George <92686556+GrudTrigger@users.noreply.github.com>
2025-12-18 11:25:24 +0300

    backend:delete training

--- backend/internal/repository/trainings/delete.go
+++ backend/internal/repository/trainings/delete.go
@@ -7,5 +7,9 @@ import (
 )
 
 func (r *Repository) Delete(ctx context.Context, data *t.DeletePayload) error {
+	_, err := r.conn.Exec(ctx, "DELETE FROM trainings WHERE id=$1", data.UUID)
+	if err != nil {
+		return err
+	}
 	return nil
 }
--- backend/internal/service/trainings/delete.go
+++ backend/internal/service/trainings/delete.go
@@ -6,6 +6,10 @@ import (
 	t "github.com/GrudTrigger/training_tracker/backend/gen/trainings"
 )
 
-func (s *Service) Delete(context.Context, *t.DeletePayload) (err error) {
+func (s *Service) Delete(ctx context.Context, data *t.DeletePayload) error {
+	err := s.repoTrainigs.Delete(ctx, data)
+	if err != nil {
+		return err
+	}
 	return nil
 }
